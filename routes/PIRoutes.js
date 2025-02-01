b4b87b8855b4a5deec1cb44da9ff8a46e9548260
Vlad <vlad.ungureanu0007@gmail.com>
2025-02-01 17:20:21 +0100

    message1

--- routes/PIRoutes.js
+++ routes/PIRoutes.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const Joi = require('joi');
 const { insertPersonalInfo, getPersonalInfo } = require('../db/personalInfo');
 
+
 // Validation Schemas
 const personalInfoSchema = Joi.object({
   userId: Joi.number().integer().positive().required(),
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config(); // Load environment variables
 const { connectDB } = require('./db/db'); // Import the DB module
+const path = require('path'); 
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -10,15 +11,22 @@ const PIRoutes = require('./routes/PIRoutes');
 
 const app = express();
 const PORT = process.env.PORT; // Use environment variable for port
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 // Middleware
-app.use(cors()); // Enable CORS
+app.use(cors({
+  origin: 'http://127.0.0.1:5500', // Allow requests from this origin
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
+app.use(express.json());
 app.use(bodyParser.json());
 
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/macros', macrosRoutes);
 app.use('/api/personal-info', PIRoutes);
+console.log("Personal Info Routes loaded!");
 
 // Default route
 app.get('/', (req, res) => {
